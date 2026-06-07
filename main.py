bc7c219da747f07d8012c46d0202887b4bf02deb
davidemantia <davide.mantia.to@icloud.com>
2026-06-07 10:22:39 +0200

    update Mac

--- main.py
+++ main.py
@@ -238,7 +238,7 @@ async def socket_endpoint(websocket: WebSocket):
 
 @app.get("/")
 async def root():
-   return RedirectResponse(url="/index.php")
+   return RedirectResponse(url="http://localhost:8080/index.php")
 
 
 
