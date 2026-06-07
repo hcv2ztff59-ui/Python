5c99fce74a9d51046b5ed93fb5641c149c276916
davidemantia <davide.mantia.to@icloud.com>
2026-06-07 10:31:27 +0200

    mac update

--- main.py
+++ main.py
@@ -238,7 +238,11 @@ async def socket_endpoint(websocket: WebSocket):
 
 @app.get("/")
 async def root():
-   return RedirectResponse(url="https://todoregistrazione-production.up.railway.app/")
+   return {
+
+        "redirect": "https://todoregistrazione-production.up.railway.app/"
+
+    }
 
 
 
