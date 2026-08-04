9c8b655aa3cc95970caa7a7846b9042aa4889cfa
davidemantia <davide.mantia.to@icloud.com>
2026-08-04 11:18:05 +0200

    update mac

--- Routers/user.py
+++ Routers/user.py
@@ -157,23 +157,33 @@ def set_mention_read(
     db.commit()
     db.refresh(mention)
     
-    tokens = db.query(NotificationToken).filter(NotificationToken.id_user_ref == mention.mentioned_user_id).all()
-    user = db.query(User).filter(User.id == current_user["id_utente"]).first()
-    for token in tokens:
-            try:
-                invia_push_notifica(
-                    token.fcm_token,
-                    user.nickname,
-                    user.id,
-                    "Notifica di lettura",
-                    f"{user.nickname} ha letto la menzione",
-                    "notify_when_read",
-                   
-                )
-            except UnregisteredError:
-                db.query(NotificationToken).filter(NotificationToken.fcm_token == token.fcm_token).delete()
-                db.commit()
+       # Determiniamo chi deve ricevere la notifica di ritorno (es. l'autore del task)
+    target_user_id = (
+        mention.created_by_user_id 
+        if current_user["id_utente"] == mention.mentioned_user_id 
+        else mention.mentioned_user_id
+    )
+
+    # Recuperiamo i token FCM dell'utente destinatario della ricevuta di lettura
+    tokens = db.query(NotificationToken).filter(NotificationToken.id_user_ref == target_user_id).all()
     
+    # Utente corrente che ha eseguito l'azione di lettura
+    user = db.query(User).filter(User.id == current_user["id_utente"]).first()
+
+    for token in tokens:
+        try:
+            invia_push_notifica(
+                token.fcm_token,
+                user.nickname,
+                user.id,
+                "Notifica di lettura",
+                f"{user.nickname} ha letto la menzione",
+                "notify_when_read",
+            )
+        except UnregisteredError:
+            db.query(NotificationToken).filter(NotificationToken.fcm_token == token.fcm_token).delete()
+            db.commit()
+
     return {
         "message": "Menzione impostata come letta con successo",
         "id": mention.id,
